8e6c1b55a6036f5cd5fab28fbc5ca76420c39b19
Călin Bogdan <calin.bogdan15@gmail.com>
2020-04-13 09:57:25 +0300

    Moved axis to the axis container

--- src/VitalsDiagram/components/AxisContainer.js
+++ src/VitalsDiagram/components/AxisContainer.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React from "react";
+import { AxisLeft } from "d3-components";
+import styled from "styled-components";
+import { scaleLinear } from "d3";
 
-export default function AxisContainer() {
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
 
-}
+const ClickableRectangle = styled.rect`
+  fill: transparent;
+  fill-opacity: 0.15;
+  z-index: -15;
+  &:hover {
+    cursor: pointer;
+    fill: red;
+  }
+`;
+
+const tempScale = scaleLinear([42, 35], [0, 299]);
+
+const AxisContainer = ({ series }) => {
+  return (
+    <Wrapper>
+      {series.map(({ id, max, min, height }) => {
+        return (
+          <svg key={id} overflow="visible" height={height - 1} width={35}>
+            <AxisLeft nested scale={scaleLinear([max, min], [0, 299])} />
+            <ClickableRectangle height={299} width={35} />
+          </svg>
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+export default AxisContainer;
--- src/VitalsDiagram/components/VitalsDiagram.js
+++ src/VitalsDiagram/components/VitalsDiagram.js
@@ -1,29 +1,17 @@
 import React, { useEffect, useContext } from "react";
-import { AxisLeft } from "d3-components";
 import { scaleLinear, line } from "d3";
 import styled from "styled-components";
-import temperatures from "../../assets/temperatures.json";
+
+
+import configs from "../../assets/seriesConfigs.json";
+import values from "../../assets/seriesValues.json";
+
 import BufferWidthContext from "../../contexts/bufferContext";
+import AxisContainer from "./AxisContainer";
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
-`;
-
-const AxisContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-`;
-
-const ClickableRectangle = styled.rect`
-  fill: transparent;
-  fill-opacity: 0.15;
-  z-index: -15;
-  &:hover {
-    cursor: pointer;
-    fill: red;
-  }
 `;
 
 const Diagram = styled.svg`
@@ -40,11 +28,11 @@ const tempScale = scaleLinear([42, 35], [0, 299]);
 const Series = () => {
   const lineAs = line()
     .y((d) => tempScale(d))
-    .x((_, i) => i * 5);
+    .x((_, i) => i * 3);
 
   return (
     <g>
-      <Line d={lineAs(temperatures.slice(0, 300))} />
+      <Line d={lineAs(values[0].values.slice(0, 800))} />
     </g>
   );
 };
@@ -53,17 +41,12 @@ const VitalsDiagram = (props) => {
   const { setSeriesNumber } = useContext(BufferWidthContext);
 
   useEffect(() => {
-    setSeriesNumber(1);
+    setSeriesNumber(configs.length);
   }, []);
 
   return (
     <Container {...props}>
-      <AxisContainer>
-        <svg overflow="visible" height={299} width={35}>
-          <AxisLeft nested scale={tempScale} />
-          <ClickableRectangle height={299} width={35} />
-        </svg>
-      </AxisContainer>
+      <AxisContainer series={configs}/>
       <Diagram width={1415} height={300}>
         <Series />
       </Diagram>
