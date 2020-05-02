95b432519bd6fed8c63dc8cefd43e80cb8ef8759
Călin Bogdan <calin.bogdan15@gmail.com>
2020-05-03 00:10:59 +0300

    Added medication order header

--- src/components/Diagram/MedicationDiagram/MedicationDiagram.js
+++ src/components/Diagram/MedicationDiagram/MedicationDiagram.js
@@ -1,8 +1,44 @@
 import React, { useContext } from "react";
 import BufferContext from "../../bufferContext";
+import styled from "styled-components";
+
+const orders = [
+  {
+    medication: "Paracetamol",
+  },
+  {
+    medication: "ParaSinus",
+  },
+];
+
+const MedicationOrderHeader = styled.div`
+  display: block;
+  max-width: ${(props) => props.width}px;
+  padding: 8px 12px;
+  background: #ddd;
+  box-sizing: border-box;
+`;
+
+const MedicationOrderTitle = styled.p`
+  margin: 0;
+`;
 
 const MedicationDiagram = () => {
-  return <div>This is the MedicationDiagram</div>;
+  const { bufferWidth } = useContext(BufferContext);
+
+  return (
+    <div>
+      {orders.map((order, index) => {
+        return (
+          <div key={index}>
+            <MedicationOrderHeader width={bufferWidth}>
+              <MedicationOrderTitle>{order.medication}</MedicationOrderTitle>
+            </MedicationOrderHeader>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MedicationDiagram;
