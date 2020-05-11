68c3180116265ca9e385086881cb4a93210662d2
Călin Bogdan <calin.bogdan15@gmail.com>
2020-05-11 17:30:15 +0300

    Fixed dragging outside of domain issue

--- src/components/timelineContext.js
+++ src/components/timelineContext.js
@@ -11,7 +11,6 @@ const TimelineContext = createContext(null);
 
 const ZOOM = "ZOOM";
 const FULL_DOMAIN_CHANGE = "FULL_DOMAIN_CHANGE";
-const DOMAIN_CHANGE = "DOMAIN_CHANGE";
 const DRAG = "DRAG";
 const SCALE_RANGE_CHANGED = "SCALE_RANGE_CHANGED";
 
@@ -29,11 +28,6 @@ function changeDomainAndScaleWithFullDomainLimit(scale, domain, fullDomain) {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case DOMAIN_CHANGE:
-      return {
-        ...state,
-        ...changeDomainAndScaleWithFullDomainLimit(state.scale, action.domain, state.fullDomain)
-      };
     case FULL_DOMAIN_CHANGE:
       return {
         ...state,
@@ -50,17 +44,20 @@ const reducer = (state, action) => {
 
     case DRAG: {
       const { movementX } = action;
-
       const x1 = state.scale(state.domain[0]);
       const x2 = state.scale(state.domain[1]);
 
       const newStart = state.scale.invert(x1 - movementX);
       const newEnd = state.scale.invert(x2 - movementX);
 
-      return {
-        ...state,
-        ...changeDomainAndScaleWithFullDomainLimit(state.scale, [newStart, newEnd], state.fullDomain)
+      if (newStart.getTime() >= state.fullDomain[0].getTime() && newEnd.getTime() <= state.fullDomain[1].getTime()) {
+        return {
+          ...state, 
+          scale: scaleTime([newStart, newEnd], state.scale.range()),
+          domain: [newStart, newEnd]
+        }
       }
+      return state;
     }
     case ZOOM:
       const { x, zoomingIn } = action;
@@ -87,11 +84,20 @@ const reducer = (state, action) => {
         newStart = leftScale.invert(0 - leftPixelsInterval * 0.1);
         newEnd = rightScale.invert(diagramWidth + rightPixelsInterval * 0.1);
       }
+      
+      // prevent the user from zooming out more than the full domain
 
-      return {
-        ...state,
-        ...changeDomainAndScaleWithFullDomainLimit(state.scale, [newStart, newEnd], state.fullDomain)
-      };
+      const fullInterval = state.fullDomain[1].getTime() - state.fullDomain[0].getTime();
+      const interval = newEnd.getTime() - newStart.getTime();
+
+      if (interval <= fullInterval) {
+        return {
+          ...state,
+          scale: scaleTime([newStart, newEnd], state.scale.range()),
+          domain: [newStart, newEnd]
+        };
+      }
+      return state;
   }
 };
 
